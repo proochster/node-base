031b0c0bf4c873b1e939228b8fd00655bd2d6c44
proochster <proochster@gmail.com>
2019-11-21 12:55:02 +0000

    + File systrm module

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const pathInfo = require('./path-info');
 const os = require('./os-info');
 const processLog = require('./process-info');
+const fsInfo = require('./filesystem-info');
 
 pathInfo(__filename);
 os();
 processLog();
+fsInfo;
