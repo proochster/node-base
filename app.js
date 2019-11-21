9073c016575eb808b8f90a84235b0c593cef0fa1
proochster <proochster@gmail.com>
2019-11-21 15:56:08 +0000

    + Event emitter class

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const os = require('./os-info');
 const processLog = require('./process-info');
 const fsInfo = require('./filesystem-info');
 
+const Subscription = require('./event-emitter');
+const emitter = new Subscription();
+
 pathInfo(__filename);
 os();
 processLog();
 fsInfo;
+
+emitter.on('userSubscribed', function(eventArg){
+    console.log("User subscribed at", eventArg.date);
+    // Do something when user subscribes
+});
+
+emitter.sendEvent()
